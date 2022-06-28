227cbae341205d47fc062f5ff18c6c0745204f1a
Blake Sweet <BSweet16>
2022-06-28 03:18:14 -0400

    Add enter key listener

--- assets/js/page-lock.js
+++ assets/js/page-lock.js
@@ -35,5 +35,16 @@ $(document).ready(function() {
       $("#securityModal").css("backdrop-filter", ""); // Blur around the modal
     }
   });
+
+  // Execute a function when the user presses a key on the keyboard
+  document.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("nameSubmit").click();
+    }
+  });
   
 });
